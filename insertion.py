fc3506cf25964770007259a45727a1a05fae3a45
ARAYNAUD <adrien.raynaud@ynov.com>
2023-01-06 16:08:38 +0100

    pour maxime bisous adrien

--- insertion.py
+++ insertion.py
@@ -1,8 +1,14 @@
 #function who insert a element in a sql db
 import sqlite3
-def insert_element(table,element):
+def insert_element(table,key,element):
+    if type(element) == str:
+        element = element
+    elif type(element) == list:
+        element = ",".join(element)
+    else :
+        element = str(element)
     con = sqlite3.connect('train.db')
     c = con.cursor()
-    c.execute("INSERT INTO (?) VALUES (?)", (table,element))
+    c.execute("INSERT INTO ? (?) VALUES (?)", (table,key,element))
     con.commit()
     con.close()
